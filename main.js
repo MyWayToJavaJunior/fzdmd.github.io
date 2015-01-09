086bc449003535f2314e1ab7365f9f77c854da79
fzdmd <fuzeg@ya.ru>
2015-01-09 09:54:54 +0400

    second

--- main.js
+++ main.js
@@ -37,3 +37,58 @@ $(document).ready(function(e) {
                 var map = new google.maps.Map(mapElement, mapOptions);
 
             };
+
+/***************** Smooth Scrolling ******************/
+
+$(function() {
+
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 2000);
+				return false;
+			}
+		}
+	});
+
+});
+
+$(document).ready(function() {
+
+	$('.wp1').waypoint(function() {
+		$('.wp1').addClass('animated fadeInLeft');
+	}, {
+		offset: '100%'
+	});
+	$('.wp2').waypoint(function() {
+		$('.wp2').addClass('animated fadeInRight');
+	}, {
+		offset: '100%'
+	});
+	$('.wp3').waypoint(function() {
+		$('.wp3').addClass('animated fadeInUp');
+	}, {
+		offset: '100%'
+	});
+	$('.wp4').waypoint(function() {
+		$('.wp4').addClass('animated fadeInDown');
+	}, {
+		offset: '100%'
+	});
+	$('.wp5').waypoint(function() {
+		$('.wp5').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp6').waypoint(function() {
+		$('.wp6').addClass('animated fadeInDown');
+	}, {
+		offset: '100%'
+	});
+
+});
